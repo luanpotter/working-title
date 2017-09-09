de1448a353804117755fec5ba76731cc9702cf53
Luan Nico <luanpotter27@gmail.com>
2017-09-09 00:37:15 -0300

    Fix connected textures once and for all

--- map.js
+++ map.js
@@ -2,7 +2,7 @@ const PIXI = require('pixi.js');
 const Block = require('./block.js');
 
 const { rand, empty, range, arrEq, inside } = require('./util.js');
-const { cos, sin, PI, floor } = Math;
+const { cos, sin, PI, floor, abs } = Math;
 
 const level = empty(200).map(_ => empty(200).map(_ => new Block(Block.GRASS)));
 
@@ -35,7 +35,6 @@ range(200*200 / 10).forEach(() => {
 	level[x][y].variation = grassVariations[rand(grassVariations.length)];
 });
 
-if (false)
 range(5 + rand(5)).forEach(() => {
 	const cx = rand(level.length);
 	const cy = rand(level[0].length);
@@ -57,21 +56,13 @@ range(5 + rand(5)).forEach(() => {
 	});
 });
 
-range(4).forEach(i => range(10).forEach(j => level[10 + i][10 + j] = new Block(Block.DIRT)));
-level[9][10 + 4] = new Block(Block.DIRT);
-level[10 + 2][9] = new Block(Block.DIRT);
-level[10 + 2][20] = new Block(Block.DIRT);
-level[14][10 + 4] = new Block(Block.DIRT);
-
-const getBorders = (i, j) => {
-	const GET = [ [+1, 0], [+1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1], [0, +1], [+1, +1] ];
-	const getIdx = idx => get(level, i + GET[idx][0], j + GET[idx][1]);
-	return range(8).filter(idx => getIdx(idx).isDirt());
-};
-
+const GET = [ [+1, 0], [+1, -1], [0, -1], [-1, -1], [-1, 0], [-1, +1], [0, +1], [+1, +1] ];
+const getIdx = (i, j, desloc) => get(level, i + desloc[0], j + desloc[1]);
+const getBorders = (i, j) => range(8).filter(idx => getIdx(i, j, GET[idx]).isDirt());
 const getDirtMetadata = (i, j) => {
 	const borders = getBorders(i, j);
 	const cross = borders.filter(i => i % 2 === 0);
+	const corners = borders.filter(i => i % 2 === 1);
 	if (cross.length === 0) {
 		return [ 0, 0 ];
 	}
@@ -80,8 +71,69 @@ const getDirtMetadata = (i, j) => {
 		return [ 1, map[cross[0]] ];
 	}
 	if (cross.length === 3) {
-		const map = { 0:2, 2:1, 4:0, 6:3 };
-		return [ 5, map[[0, 2, 4, 6].find(el => !cross.includes(el))] ];
+		const oddOneOut = [0, 2, 4, 6].find(el => !cross.includes(el));
+		const actualCorners = corners.filter(c => abs(c - oddOneOut) > 1 && abs(c - oddOneOut) < 7);
+		if (actualCorners.length === 0) {
+			const map = { 0:3, 2:2, 4:1, 6:0 };
+			return [ 7, map[oddOneOut]];
+		} else if (actualCorners.length === 1) {
+			const map = { 0:3, 2:2, 4:1, 6:0 };
+			const myCorner = actualCorners[0];
+			let shouldFlip = GET[oddOneOut][0] == 0 ? GET[myCorner][0] : -GET[myCorner][1];
+			if (oddOneOut === 6 || oddOneOut === 0) {
+				shouldFlip *= -1;
+			}
+			const flip = shouldFlip === -1 ? 6 : 14;
+			if (oddOneOut === 0) {
+				return [ flip, 3 ];
+			} else if (oddOneOut === 2) {
+				return [ flip, 2 ];
+			} else if (oddOneOut === 4) {
+				return [ flip, 1 ];
+			} else if (oddOneOut === 6) {
+				return [ flip, 0 ];
+			}
+		} else {
+			const map = { 0:2, 2:1, 4:0, 6:3 };
+			return [ 5, map[oddOneOut] ];
+		}
+	}
+	if (cross.length === 2) {
+		const id = cross.sort().join('+');
+		if (id === '0+4') {
+			return [ 2, 0 ];
+		}
+		if (id === '2+6') {
+			return [ 2, 1 ];
+		}
+		const desloc = GET[cross[0]].map((el, i) => el + GET[cross[1]][i]);
+		const centralDirt = getIdx(i, j, desloc).isDirt();
+		const map = { '0+2': 1, '0+6': 2, '2+4': 0, '4+6': 3 };
+		return [ centralDirt ? 4 : 3, map[id] ];
+	}
+	// 4
+	if (corners.length === 3) {
+		const map = { 1:1, 3:0, 5:3, 7:2 };
+		return [ 9, map[[1, 3, 5, 7].find(el => !corners.includes(el))] ];
+	}
+	if (corners.length === 2) {
+		const id = corners.sort().join('+');
+		return {
+			'1+3': [13, 3],
+			'1+5': [12, 1],
+			'1+7': [13, 0],
+			'3+5': [13, 2],
+			'3+7': [12, 1],
+			'5+7': [13, 1]
+		}[id];
+	}
+	if (corners.length === 1) {
+		const actualCorner = corners[0];
+		const map = { 1:0, 3:3, 5:2, 7:1 };
+		return [ 11, map[actualCorner] ];
+	}
+	if (corners.length === 0) {
+		return [ 10, 0 ];
 	}
 	return [ 8, 0 ];
 };
